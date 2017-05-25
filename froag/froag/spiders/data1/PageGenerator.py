67b1827cf59cdaab68e88e36de5930f3595a4aaf
floatage <1300730251@qq.com>
2017-05-25 22:32:14 +0800

    start write the system interactiving interface

---
+++ froag/froag/spiders/data1/PageGenerator.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib.request as ur
+import re
+
+class PageGenerator:
+    def getPage(self, id):
+        pass
+    
+    def __pageSearch__(self, id):
+        pass
+    
+    def __pageImageGenete__(self, text):
+        pass
+    
+    def __pageInforGenete__(self, text, template):
+        pass
+    
