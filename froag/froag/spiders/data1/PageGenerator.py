79f83dfc491b34672c1283985406926dc19f8a2f
floatage <1300730251@qq.com>
2017-05-29 22:43:08 +0800

    devolep the parser of the page template

--- froag/froag/spiders/data1/PageGenerator.py
+++ froag/froag/spiders/data1/PageGenerator.py
@@ -66,68 +66,97 @@ class PageGenerator:
             file.close()
         return text
     
-    def __generatePage__(self, param):
-        cursor = self.connOR.cursor()
+    def __generatePage__(self, params):
         template = json.loads(self.template)
-        
-        datas = list(template['data'].items())
-        dataQueue = map(lambda x:x[0], datas) 
-        while(len(dataQueue) > 0):
-            dataName = dataQueue.pop(0)
-            depends = re.match(r"\[.*\]", datas['sql'])
-            if depends:
-                pass
-            else:
-                cursor.execute()
-        
-        result = cursor.execute(self.SQL_GET_PAGE, param['pageid'])
-        item = result.fetchone()
-        if item != None:
-            item = list(item)
-            colDict, index = {}, 0
-            for col in result.description:
-                colDict[col[0]] = index
-                index += 1
-            result = cursor.execute(self.SQL_GET_SIMILARPAGE, item[colDict['mSource']])
-            similarPages = result.fetchall()
-        
-            text = ''
-            self.__pageImageGenete__(item, colDict, self.template)
-            text = self.__pageInforGenerate__(item, similarPages, colDict, self.template)
+        paramsDict = self.__templateDataFetch(self.connOR.cursor(), params, template['data'])
+        self.__templateRuleApply__(paramsDict, template['rule'])
+        page = self.__templateStyleApply__(paramsDict, template['style'])
             
-            filename = item[colDict['mSource']].split('//')[-1]
-            storePath = self.pageStoreDir + filename
-            pageFile = open(storePath, 'w')
-            pageFile.write(text)
-            pageFile.close()
+#         filename = item[colDict['mSource']].split('//')[-1]
+#         storePath = self.pageStoreDir + filename
+#         pageFile = open(storePath, 'w')
+#         pageFile.write(text)
+#         pageFile.close()
+#             
+#         try:
+#             self.connS.execute(self.SQL_INSERT_PAGEDICT, (param['pageid'], storePath, 1))
+#             self.connSL.commit()
+#         except sqlite3.IntegrityError:
+#             print('page:%d path insert error' % param['pageid'])
+#             pass
             
-            try:
-                self.connS.execute(self.SQL_INSERT_PAGEDICT, (pageId, storePath, 1))
-                self.connSL.commit()
-            except sqlite3.IntegrityError:
-                print('page:%d path insert error' % pageId)
-                pass
-            
-        return text
+        return page
     
-    def __pageImageGenete__(self, item, colDict, template, way='web'):
-        contentTree = BeautifulSoup(item[colDict['mContent']], self.DEFAULT_PARSER) 
-        handler = self.__imageHandleWeb__ if way=='web' else self.__imageHandleWeb__
-        for image in contentTree.find_all():
-            handler(image)
-        item[colDict['mContent']] = contentTree.find('div').prettify()
+    def __templateDataFetch(self, cursor, params, templateDataStatements):
+        valuePattern = re.compile(r"\[\[.*?\]\]")
+        
+        paramsDict = {}
+        for key,value in params.items():
+            paramsDict['params.' + key] = value
+            
+        dataStatemetNames, dataStatemetLen = list(templateDataStatements.keys()), len(templateDataStatements)
+        statemetSeq, dataStatemetStack = ['params'], []
+        while (len(statemetSeq) == dataStatemetLen):
+            if (len(dataStatemetStack) == 0):
+                dataStatemetStack.append((dataStatemetNames.pop(), 'params'))
+                
+            dataStatemet = dataStatemetStack[-1]
+            depends = valuePattern.findall(templateDataStatements[dataStatemet[0]]['sql'])
+            depends = set(map(lambda x:x[2:-2].split('.')[0], depends))
+            
+            isFinished = True
+            for depend in depends:
+                if depend == dataStatemet[1] and depend != 'params':
+                    raise ValueError('statement cross reference')
+                if depend not in statemetSeq:
+                    isFinished = False
+                    dataStatemetStack.append((depend, dataStatemet[0]))
+            if isFinished:
+                sta = dataStatemetStack.pop()
+                if sta[0] not in statemetSeq:
+                    statemetSeq.append(sta[0])
+        
+        for statement in statemetSeq:
+            sql = templateDataStatements[statement]['sql']
+            depends = valuePattern.findall(sql)
+            
+            rs = None
+            if len(depends) > 0: 
+                sqlValues = []
+                sql = valuePattern.sub('?', sql)
+                for depend in depends:
+                    depend = depend[2:-2]
+                    sqlValues.append(paramsDict[depend])
+                rs = cursor.execute(sql, sqlValues).fetchall()
+            else:
+                rs = cursor.execute(sql).fetchall()
+                
+            if len(rs) > 0:
+                for index, col in enumerate(cursor.description):
+                    paramsDict['%s.%s' % (statement, col[0])] = list(map(lambda x:x[index], rs))
+        
+        return paramsDict
+    
+    def __templateRuleApply__(self, paramsDict, templateRules):
+        if "imgHandler" in templateRules:
+            cols = json.loads(templateRules["imgHandler"])
+            handler = self.__imageHandleWeb__ if templateRules["imgHandler"]=='web' else self.__imageHandleWeb__
+            for col in cols:
+                colName = col[2:-2]
+                for row in paramsDict[colName]:
+                    handler(row)
+    
+    def __templateStyleApply__(self, paramsDict, templateStyle):
+        page = ''
+        return page
             
     def __imageHandleLocal__(self, imgTag, template):
         pass
     
-    def __imageHandleWeb__(self, imgTag, template):
+    def __imageHandleWeb__(self, imgTag):
         pos = imgTag['src'].find(':')
         if pos != -1:
             imgTag['src'] = 'http' + imgTag['src'][pos:]
-            
-    def __pageInforGenerate__(self, item, similarPages, colDict, template):
-        template = json.loads(template)
-        template['layout']
     
 class PicDownloader:
     def __init__(self):
--- froag/froag/spiders/data1/functionTest.py
+++ froag/froag/spiders/data1/functionTest.py
@@ -1,33 +1,137 @@
 #encoding=utf-8
-import cx_Oracle, os
-
+import cx_Oracle, os, re, json
+import urllib.request as ur
 
 if __name__ == '__main__':
-    os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
-    DB_CONNECT_STRING_ORACLE = 'foragCollecter_1/foragCollecter@10.18.50.229/orcl'
-#     SQL_ADD_NEW_ITEM = '''INSERT INTO foragOwner.MsgTable(mId,mSource,mTitle,mIntro,mPic,mTags,mAuthor,mContent,mPublishTime, \
-#         mCollectTime,mLikeCount,mDislikeCount,mCollectCount,mTransmitCount) \
-#         VALUES(foragOwner.MID_SEQ.NEXTVAL,'%s','%s','%s','%s','%s','%s','%s',to_date('%s','yyyy-mm-dd hh24:mi:ss'),sysdate,0,0,0,0)
-#     '''
-    SQL_ADD_NEW_ITEM = '''INSERT INTO foragOwner.MsgTable(mId,mSource,mTitle,mIntro,mPic,mTags,mAuthor,mContent,mPublishTime,\
-mCollectTime,mLikeCount,mDislikeCount,mCollectCount,mTransmitCount)\
-VALUES(foragOwner.MID_SEQ.NEXTVAL,'dawfdsefadawwda','daw','daw','daw','daw','daw','daw',to_date('2017-05-17 09:55:00','yyyy-mm-dd hh24:mi:ss'),sysdate,0,0,0,0)'''
-
-    connection = cx_Oracle.connect(DB_CONNECT_STRING_ORACLE)
-    print('get connection')
-    cursor = connection.cursor()
-    print('get cursor')
-    SQL_ADD_NEW_ITEM = "INSERT INTO foragOwner.SimilarUrl(sourceUrl,similarUrl) VALUES('中dcdaw文','中文')"
-    print(SQL_ADD_NEW_ITEM)
-    cursor.execute(SQL_ADD_NEW_ITEM)
-    print('sql execute')
-    connection.commit()
-    print('conmmit success')
-    result = cursor.execute('select * from foragOwner.similarurl').fetchall()
- 
-    for item in result:
-        for field in item:
-            if isinstance(field, str):
-                print(field)
-    connection.close()
-    print('db close')
+#     os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+#     DB_CONNECT_STRING_ORACLE = 'foragCollecter_1/foragCollecter@10.18.50.229/orcl'
+# #     SQL_ADD_NEW_ITEM = '''INSERT INTO foragOwner.MsgTable(mId,mSource,mTitle,mIntro,mPic,mTags,mAuthor,mContent,mPublishTime, \
+# #         mCollectTime,mLikeCount,mDislikeCount,mCollectCount,mTransmitCount) \
+# #         VALUES(foragOwner.MID_SEQ.NEXTVAL,'%s','%s','%s','%s','%s','%s','%s',to_date('%s','yyyy-mm-dd hh24:mi:ss'),sysdate,0,0,0,0)
+# #     '''
+#     SQL_ADD_NEW_ITEM = '''INSERT INTO foragOwner.MsgTable(mId,mSource,mTitle,mIntro,mPic,mTags,mAuthor,mContent,mPublishTime,\
+# mCollectTime,mLikeCount,mDislikeCount,mCollectCount,mTransmitCount)\
+# VALUES(foragOwner.MID_SEQ.NEXTVAL,'dawfdsefadawwda','daw','daw','daw','daw','daw','daw',to_date('2017-05-17 09:55:00','yyyy-mm-dd hh24:mi:ss'),sysdate,0,0,0,0)'''
+#   
+#     connection = cx_Oracle.connect(DB_CONNECT_STRING_ORACLE)
+#     print('get connection')
+#     cursor = connection.cursor()
+#     print('get cursor')
+#     SQL_ADD_NEW_ITEM = "INSERT INTO foragOwner.SimilarUrl(sourceUrl,similarUrl) VALUES('中dcdaw文','中文')"
+# #     print(SQL_ADD_NEW_ITEM)
+# #     cursor.execute(SQL_ADD_NEW_ITEM)
+# #     print('sql execute')
+# #     connection.commit()
+# #     print('conmmit success')
+#     result = cursor.execute('select * from foragOwner.MsgTable where rownum<=2').fetchall()
+#     paramsDict = {}
+#     for index, col in enumerate(cursor.description):
+#         paramsDict['%s.%s' % ('item', col[0])] = list(map(lambda x:x[index] if x[index]!=None else '', result))
+#     print(paramsDict)
+#     for item in result:
+#         for field in item:
+#             print(field)
+#     connection.close()
+# #     data = ur.urlopen('http://himg2.huanqiu.com/attachment2010/2017/0224/14/27/20170224022730835.jpg')
+# #     data = data.read()
+# #     pic = open('20170224022730835.jpg', 'wb+')
+# #     pic.write(data)
+# #     pic.close()
+#     print('db close')
+    text = '''<article class="item">
+                      <header class="item-head">
+                        <h3 class="item-title">[[MTITLE]]</h3>
+                        <section class="item-meta">
+                          <span class="item-author">作者：<a href="#">[[MAUTHOR]]</a></span> •
+                          <time class="item-date">[[MTIME]]</time>
+                        </section>
+                      </header>
+                      <section class="item-content">[[MCONTENT]]</section>
+                      <footer class="item-foot">
+                        <section class="item-souce"> 
+                          <span class="item-source">
+                            <a  href="[[MSOURCE]]">原网站</a>
+                          </span>
+                        </section>
+                        <section class="item-tag">
+                          <span class="label" repeat="true" statement="class:['label-danger','label-success','label-info','label-warning','label-primary']">[[MTAG]]
+                          </span>
+                        </section>
+                      </footer>
+                    </article>'''
+    for value in re.finditer(r'\[\[.*?\]\]', text):
+        print('%s %s' % (value, value.span()))
+        text.replace(text[value.start():value.end()], '')
+          
+    print(text)
+    text = r'''
+        {
+                    "data": {
+                            "item": {
+                                "sql": "select * from msgtable where mid=[[params.pageid]]"
+                            }, 
+                            "item-similar": {
+                                "sql": "select * from msgtable,(select similarurl from similarurl where sourceurl=[[item.MSOURCE]]) s where msource=similarurl"
+                            }
+                    },
+                    "rule": {
+                        "imgHandler": {
+                            "col": ["[[item.MCONTENT]]"],
+                            "way": "web"
+                        }
+                    },
+                    "style": {
+                        "css":"xx.css",
+                        "layout": 
+                            "<article class='item'>
+                              <header class='item-head'>
+                                <h3 class='item-title'>[[MTITLE]]</h3>
+                                <section class='item-meta'>
+                                  <span class='item-author'>作者：<a href='#'>[[MAUTHOR]]</a></span> •
+                                  <time class='item-date'>[[MTIME]]</time>
+                                </section>
+                              </header>
+                              <section class='item-content'>[[MCONTENT]]</section>
+                              <footer class='item-foot'>
+                                <section class='item-souce'> 
+                                  <span class='item-source'>
+                                    <a  href='[[MSOURCE]]'>原网站</a>
+                                  </span>
+                                </section>
+                                <section class='item-tag'>
+                                  <span class='label' repeat='true' statement='class:[\"label-danger\",\"label-success\",\"label-info\",\"label-warning\",\"label-primary\"]'>[[MTAG]]
+                                  </span>
+                                </section>
+                              </footer>
+                            </article>'
+                            <section class='item-similar'>
+                                <div class='table-responsive'>
+                               <table class='table table-hover'>
+                                 <tr repeat='true'>
+                                   <td class='item-text' style='width:40%;'>[[MTITLE]]</td>
+                                   <td style='width:25%;'>
+                                      <div class='progress progress-xs'>
+                                        <div class='progress-bar progress-bar-danger' style='width:[[MHOT]]'></div>
+                                      </div>
+                                    </td>
+                                    <td class='item-tag hidden-xs' style='width:35%;'>
+                                      <span class='label' repeat='true' statement='class:[\"label-danger\",\"label-success\",\"label-info\",\"label-warning\",\"label-primary\"]'>[[MTAG]]
+                                        </span>
+                                    </td>
+                                 </tr>
+                                </table>
+                             </div>
+                            </section>"
+                    }
+                 
+                }
+    '''
+    text = re.sub('\s', "", text)
+    print(text)
+    print(json.loads(text))
+    cols = json.loads(r'["[[item.MCONTENT]]", "[[item.MCONTENT]]"]')
+    for col in cols:
+        colName = col[2:-2]
+        print(colName)
+    testDict = {'img':5}
+    print(testDict['imgs'])
