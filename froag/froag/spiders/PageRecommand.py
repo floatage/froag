3fc6b4d8ce4d722d6d5033819f150ead52acc410
floatage <1300730251@qq.com>
2017-06-16 23:13:21 +0800

    add the PageRecommand.py

---
+++ froag/froag/spiders/PageRecommand.py
@@ -0,0 +1,23 @@
+#encoding=utf8
+
+import json
+import ForagInterfaceServer
+
+# 兴趣占比(结果筛选)，文章ID(协同过滤)，标签评价(新颖性)，加上用户信息，上下文(日期、地点)。
+class UserProfileGenerator:
+    pass
+
+class UserMsgMatcher_BaseFeature:
+    pass
+
+class UserMsgMatcher_BaseCollaboration:
+    pass
+
+class ResultSetGenerator:
+    pass
+
+class ResultSetFilter:
+    pass
+
+class ResultSetRanker:
+    pass
